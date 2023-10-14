12776de1c159b74983f58867d6c5dfd582a97de1
Volatery <tmbailey0304@gmail.com>
2023-10-14 03:13:09 -0500

    Change car button

--- src/components/Reserve.jsx
+++ src/components/Reserve.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import { useCarContext } from './CarContext'
+import { Link } from 'react-router-dom';
 
 const Reserve = () => {
   useEffect(() => {
@@ -14,6 +15,7 @@ const Reserve = () => {
       <h1>{selectedCar.price}</h1>
       <h1>{selectedCar.id}</h1>
       <img src={selectedCar.img}></img>
+      <Link to='/'><button className='bg-orange-600 px-4 py-2 text-white font-bold hover:scale-105 shadow-md hover:shadow-lg shadow-orange-600 hover:shadow-orange-600 duration-300'>Change Car</button></Link>
     </div>
   )
 }
